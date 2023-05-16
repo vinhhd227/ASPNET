f49a8e296f797c11209f8c6f51309c6d977d2d17
vinhhd227 <vinhhd227@gmail.com>
2023-05-16 16:02:50 +0700

    Map - Request - Response

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 Console.WriteLine("Start app");
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,31 +32,96 @@ var app = builder.Build();
 //     app.UseExceptionHandler("/Home/Error");
 //     app.UseHsts();
 // }
-app.UseRouting();
 app.UseStaticFiles();
+app.UseRouting();
 
-app.UseFirstMiddleWare();
-app.UseSecondMiddleWare();
 app.UseEndpoints((endpoints) =>
 {
-    endpoints.MapGet("/product", async (c) => await c.Response.WriteAsync("Trang san pham"));
-    endpoints.MapGet("/home", async (c) => await c.Response.WriteAsync("Trang chu"));
-    endpoints.MapGet("/about", async (c) => await c.Response.WriteAsync("Trang san pham"));
+    endpoints.MapGet("/", async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        var html = HtmlHelper.HtmlDocument("Xin chao ASP", menu + HtmlHelper.HtmlTrangchu());
+        await c.Response.WriteAsync(html);
+    });
+    endpoints.MapGet("/RequestInfo", async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        string info = RequestProcess.RequestInfo(c.Request).HtmlTag("div", "container");
+        var html = HtmlHelper.HtmlDocument("Thong tin Request", menu + info);
+        await c.Response.WriteAsync(html);
+    });
+    endpoints.MapGet("/Encoding", async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        var html = HtmlHelper.HtmlDocument("Thong tin Request", menu);
+        await c.Response.WriteAsync(html);
+    });
+    endpoints.MapGet("/Cookies/{*action}", async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        var action = c.GetRouteValue("action") ?? "read";
+        string message = "";
+        if (action.ToString() == "write")
+        {
+
+            var option = new CookieOptions()
+            {
+                Path = "/",
+                Expires = DateTime.Now.AddDays(1)
+            };
+            c.Response.Cookies.Append("masanpham", "928356392486524985672", option);
+            message = "Cookie được ghi";
+        }
+        else
+        {
+            // Lấy danh sách các Header và giá trị  của nó, dùng Linq để lấy
+            var listcokie = c.Request.Cookies.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+            message = string.Join("", listcokie).HtmlTag("ul");
+        }
+        var huongdan = "<a class=\"btn btn-danger me-3\" href=\"/Cookies/read\">Doc cookies</a><a class=\"btn btn-success\" href=\"/Cookies/write\">Ghi cookies</a>";
+        huongdan = huongdan.HtmlTag("div", "container mt-4");
+        message = message.HtmlTag("div", "alert alert-danger");
+        var html = HtmlHelper.HtmlDocument("Thong tin Cookies", menu + huongdan + message);
+        await c.Response.WriteAsync(html);
+    });
+    endpoints.MapGet("/Json", async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        var p = new
+        {
+            TenSP = "Dong ho abc",
+            Gia = 5000000,
+            NgaySX = new DateTime(2000, 12, 31)
+        };
+        c.Response.ContentType = "application/json";
+        var json = JsonConvert.SerializeObject(p);
+
+
+        // var html = HtmlHelper.HtmlDocument("Thong tin Json", menu);
+        await c.Response.WriteAsync(json);
+    });
+    endpoints.MapMethods("/Form", new string[] { "POST", "GET" }, async (c) =>
+    {
+        var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), c.Request);
+        string form = RequestProcess.ProcessForm(c.Request).HtmlTag("div", "container");
+        var html = HtmlHelper.HtmlDocument("Thong tin Form", menu + form);
+        await c.Response.WriteAsync(html);
+    });
 });
 
-// app.UseStatusCodePages();
-app.Map("/admin", (appAdmin) =>
-{
-    appAdmin.UseRouting();
-    appAdmin.UseEndpoints((endpoints) =>
-    {
-        endpoints.MapGet("/product", async (c) => await c.Response.WriteAsync("Trang quan ly san pham"));
-        endpoints.MapGet("/home", async (c) => await c.Response.WriteAsync("Trang chu admin"));
-        endpoints.MapGet("/user", async (c) => await c.Response.WriteAsync("Trang san quan ly user"));
-    });
-    appAdmin.Run(async (context) => await context.Response.WriteAsync("Terminal MiddleWare Admin"));
-});
-app.Run(async (context) => await context.Response.WriteAsync("Terminal MiddleWare"));
+// app.UseStatusCodePages();  
+// app.Map("/admin", (appAdmin) =>
+// {
+//     appAdmin.UseRouting();
+//     appAdmin.UseEndpoints((endpoints) =>
+//     {
+//         endpoints.MapGet("/product", async (c) => await c.Response.WriteAsync("Trang quan ly san pham"));
+//         endpoints.MapGet("/home", async (c) => await c.Response.WriteAsync("Trang chu admin"));
+//         endpoints.MapGet("/user", async (c) => await c.Response.WriteAsync("Trang san quan ly user"));
+//     });
+//     appAdmin.Run(async (context) => await context.Response.WriteAsync("Terminal MiddleWare Admin"));
+// });
+// app.Run(async (context) => await context.Response.WriteAsync("Terminal MiddleWare"));
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
